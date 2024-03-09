8d58785e4f56291fd342b6be86a00f45f6444672
kassiopereirabm <kassio.lima.p@gmail.com>
2024-03-09 04:09:12 -0300

    análie completa dos dados

--- analise.py
+++ analise.py
@@ -1,117 +1,112 @@
-import csv
-import json
 import pandas as pd
-from sklearn.model_selection import train_test_split
+from service import AiModel, AiModelService
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+def obterMaxCaracteresFromModel() -> int:
+    safetyValue = 40
+    tokens = AiModel.tokenizer.encode_plus(AiModelService.promptGigante, max_length=AiModel.getModelAttr('tokens'), truncation=True, return_tensors="pt")
+    return len(AiModel.tokenizer.decode(tokens["input_ids"][0], skip_special_tokens=True)) - safetyValue
+
+def sumarizeModel(indexModel: int):
+    df = pd.read_json("datasets/database.json")
+    
+    AiModel.init(modelIndex=indexModel)
+    AiModelService.init(testing=True)
+    
+    maxLen = obterMaxCaracteresFromModel()
+    
+    df['length'] = df['text'].apply(len)
+    
+    onLimit = len(df[df['length'] <= maxLen])
+    outLimit = len(df[df['length'] > maxLen])
+    total = onLimit + outLimit
+    
+    return {
+        "modelo": AiModel.getModelAttr('name'),
+        "tamanhoMaximoPrompt": maxLen,
+        "promptsTotais": total,
+        "promptsNoLimite": onLimit,
+        "promptsForaDoLimite": outLimit,
+        "percentualPromptsNoLimite": str(round((onLimit / total) * 100, 2)) + "%",
+        "percentualPromptsForaDoLimite": str(round((outLimit / total) * 100, 2)) + "%"
+    }
 
 
-jsonDatasetTrainName = "datasets/database.json"  # dados extraídos do banco em 07/03/2024
-datasetTrainName = "datasets/dataset-train.csv"
-datasetEvaluationName = "datasets/dataset-evaluation.csv"
-percentual_validacao = 0.2
-sampleSize = 100
+def analiseModels():
+    AiModel.modelOptions.sort(key=lambda x: x["tokens"])
+    sumariesModel = [sumarizeModel(i) for i in range(len(AiModel.modelOptions))]
+    
+    for sumary in sumariesModel:
+        valor_total = sumary['promptsTotais']
+        classe1 = sumary['promptsNoLimite']
+        classe2 = sumary['promptsForaDoLimite']
+        
+        labels = ['Demandas com caracteres no limite (' + str(sumary['promptsNoLimite']) + ')',
+                  'Demandas com caracteres fora do limite (' + str(sumary['promptsForaDoLimite']) + ')']
+        sizes = [classe1, classe2]
+        
+        plt.figure(figsize=(10, 6))
+        plt.pie(sizes, labels=labels, autopct='%1.1f%%', startangle=90, colors=['lightgreen', 'lightcoral'])
+        plt.title("IA: " + sumary['modelo'] + " | Limite de caracteres: " + str(sumary['tamanhoMaximoPrompt']))
+        
+        total_text = f'Demandas: {valor_total}'
+        plt.text(0, 0, total_text, ha='center', va='center', fontsize=12, color='black', fontweight='bold')
+        
+        plt.show()
 
 
-def preprocess_text(text):
-    return json.dumps(text)[1:-1].replace('"', '\\"')
+def count_characters(text):
+    return len(str(text))
 
 
-def montarDataSetTrain(jsonSource, sourceColumnsToPreprocess, datasetName, teste=False):
-    global sampleSize
+def sumarizeDataset():
+    df = pd.read_json("datasets/database.json")
+    df['text_length'] = df['text'].apply(count_characters)
+    
+    classSize = 1000
+    
+    # Definir intervalos de classes (por exemplo, intervalos de classSize caracteres)
+    intervalos = range(0, df['text_length'].max() + classSize, classSize)
+    
+    # Criar coluna 'classe' com base nos intervalos
+    df['classe'] = pd.cut(df['text_length'], bins=intervalos, right=False)
+    
+    # Contar observações por classe
+    contagem_por_classe = df['classe'].value_counts().sort_index()
+    
+    # Filtrar classes com menos de 10 observações
+    classes_validas = contagem_por_classe[contagem_por_classe >= 10].index
+    
+    # Filtrar DataFrame para incluir apenas observações nas classes válidas
+    df_filtrado = df[df['classe'].isin(classes_validas)]
+    
+    # Remover classes com menos de 10 observações dos intervalos
+    intervalos_filtrados = df_filtrado['classe'].unique()
+    intervalos_filtrados = sorted(intervalos_filtrados, key=lambda x: x.left)
+    
+    # Configurar o estilo seaborn
+    sns.set(style="whitegrid")
+    
+    # Criar um gráfico de barras usando seaborn
+    plt.figure(figsize=(10, 6))
+    ax = sns.countplot(x=df_filtrado['classe'], color='blue', order=intervalos_filtrados)
+    
+    # Adicionar rótulos e título
+    # plt.xlabel('Comprimento do Texto (Classes de 1000 caracteres)')
+    plt.ylabel('Contagem de Demandas')
+    plt.title('Distribuição do Comprimento do Texto por Classes de ' + str(classSize) + ' caracteres')
+    
+    for p, label in zip(ax.patches, contagem_por_classe):
+        ax.annotate(label, (p.get_x() + p.get_width() / 2., p.get_height()), ha='center', va='center', xytext=(0, 10),
+                    textcoords='offset points')
+        
+    # Exibir o gráfico de barras
+    plt.xticks(rotation=0, ha='center')  # Rotacionar os rótulos do eixo x para melhor legibilidade
+    
+    plt.show()
+    
 
-    df = pd.read_json(jsonSource)
-
-    if teste:
-        df = df.sample(sampleSize)
-
-    if sourceColumnsToPreprocess is not None:
-        for coluna_texto in sourceColumnsToPreprocess:
-            df[coluna_texto] = df[coluna_texto].apply(preprocess_text)
-
-    df.to_csv(datasetName, index=False, quoting=csv.QUOTE_ALL, encoding='utf-8')
-
-    print("\nDataset csv gerado com sucesso! (" + datasetName + ")\n")
-
-
-def montarDataSetEvaluation(datasetName, evaluationDatasetName, column_class):
-    global percentual_validacao
-
-    # Carregue o conjunto de dados de treino
-    df_treino = pd.read_csv(datasetName, encoding='utf-8')
-
-    contagem_classes = df_treino[column_class].value_counts()
-    classes_com_apenas_um_membro = contagem_classes[contagem_classes == 1].index.tolist()
-
-    # remove do dataframe de treino os elementos que pertencem à classes com apenas 1 membro
-    df_treino = df_treino[~df_treino[column_class].isin(classes_com_apenas_um_membro)]
-
-    # Use train_test_split para dividir o conjunto de treino em treino e validação
-    df_treino, df_validacao = train_test_split(df_treino, test_size=percentual_validacao, stratify=df_treino[column_class])
-
-    # Garanta que o conjunto de validação tenha o mesmo número de classes que o conjunto de treino
-    classes_validacao = df_validacao[column_class].unique()
-    df_treino = df_treino[df_treino[column_class].isin(classes_validacao)]
-
-    verificarNumeroDeClasses(df_treino, df_validacao, column_class)
-
-    # Salve os conjuntos de treino e validação em arquivos CSV separados
-    df_treino.to_csv(datasetName, index=False, quoting=csv.QUOTE_ALL, encoding='utf-8')
-    df_validacao.to_csv(evaluationDatasetName, index=False, quoting=csv.QUOTE_ALL, encoding='utf-8')
-
-    print("Datasets separados com sucesso!")
-
-
-def montarDatasets():
-    global jsonDatasetTrainName
-    global datasetTrainName
-    global datasetEvaluationName
-
-    montarDataSetTrain(jsonDatasetTrainName, ['text'], datasetTrainName, False)
-    verificarConsistenciaDataSet(datasetTrainName)
-    montarDataSetEvaluation(datasetTrainName, datasetEvaluationName, "target")
-    verificarConsistenciaDataSet(datasetEvaluationName)
-
-    montarDataSetTrain(jsonDatasetTrainName, ['text'], parseToTesteName(datasetTrainName), True)
-    verificarConsistenciaDataSet(parseToTesteName(datasetTrainName))
-    montarDataSetEvaluation(parseToTesteName(datasetTrainName), parseToTesteName(datasetEvaluationName), "target")
-    verificarConsistenciaDataSet(parseToTesteName(datasetEvaluationName))
-
-
-def verificarConsistenciaDataSet(datasetName):
-    dataFrame = pd.read_csv(datasetName, encoding='utf-8')
-
-    for index, row in dataFrame.iterrows():
-        if len(row) != 2:
-            raise Exception(f"Erro na linha {index}: Número incorreto de colunas - {len(row)} colunas encontradas | {datasetName}")
-
-        for column, value in row.items():
-            if type(value) != type(""):
-                raise Exception(f"Tipo na linha {index}, coluna {column}: {type(value)} | {datasetName}")
-
-    if dataFrame.isnull().any().any():
-        raise Exception(f"O DataFrame contém valores nulos | {datasetName}")
-
-    if dataFrame.duplicated().any():
-        raise Exception(f"O DataFrame contém linhas duplicadas | {datasetName}")
-
-
-def parseToTesteName(name: str):
-    return name.split("/")[0] + "/teste/" + name.split("/")[1].split(".")[0] + "-teste." + name.split(".")[1]
-
-
-def verificarNumeroDeClasses(dataframe1, dataframe2, column_class):
-
-    num_classes_dataframe1 = len(dataframe1[column_class].unique())
-    num_classes_dataframe2 = len(dataframe2[column_class].unique())
-
-    str_num_classes_dataframe1 = "Classes dataframe_1: " + str(num_classes_dataframe1)
-    str_num_classes_dataframe2 = "Classes dataframe_2:  " + str(num_classes_dataframe2)
-
-    if num_classes_dataframe2 == num_classes_dataframe1:
-        print(str_num_classes_dataframe1)
-        print(str_num_classes_dataframe2)
-    else:
-        raise Exception(f"A quantidade de classes está ERRADA.\nA quantidade de classes em ambos os dataframes devem ser IGUAIS!\n{str_num_classes_dataframe1}\n{str_num_classes_dataframe2}")
-
-
-if __name__ == '__main__':
-    montarDatasets()
+if __name__ == "__main__":
+    sumarizeDataset()
+    analiseModels()
