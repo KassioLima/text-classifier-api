ef9c228968603bf381d7f6dd233f3bfbeaa53ae5
kassiopereirabm <kassio.lima.p@gmail.com>
2024-03-13 21:55:34 -0300

    endpoints dedicados

--- main.py
+++ main.py
@@ -3,13 +3,14 @@ from CustomResponse import Response
 from fastapi import FastAPI
 
 from printUtils import greenText
-from service import AiModelService as AMS, AiModelProduto, AiModelAssunto, AiModelProdutoAssunto
+from service import AiModelService as AMS, AiModelProduto, AiModelAssunto, AiModelProdutoAssunto, AiModelTipoDemanda
 
 app = FastAPI()
 
 @app.on_event("startup")
 async def startup_event():
     # AiModelProdutoAssunto.init(modelIndex=0)
+    AiModelTipoDemanda.init(modelIndex=0)
     AiModelProduto.init(modelIndex=0)
     AiModelAssunto.init(modelIndex=0)
     AMS.init(testing=False)
@@ -18,6 +19,30 @@ async def startup_event():
 @app.get("/apiStatus")
 async def apiStatus():
     return Response("API funcionando corretamente")
+
+@app.post("/classify/tipoDemanda")
+async def classifyTipoDemanda(request: dict):
+    try:
+        result = await AMS.classifyTipoDemanda(request['prompt'])
+        return Response(result)
+    except:
+        return Response(traceback.format_exc(), 500)
+    
+@app.post("/classify/produto")
+async def classifyProduto(request: dict):
+    try:
+        result = await AMS.classifyProduto(request['prompt'])
+        return Response(result)
+    except:
+        return Response(traceback.format_exc(), 500)
+
+@app.post("/classify/assunto")
+async def classifyAssunto(request: dict):
+    try:
+        result = await AMS.classifyAssunto(request['prompt'])
+        return Response(result)
+    except:
+        return Response(traceback.format_exc(), 500)
 
 @app.post("/classify")
 async def classify(request: dict):
